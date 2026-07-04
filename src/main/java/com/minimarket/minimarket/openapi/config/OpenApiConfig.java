2aadb9e22600d800ab188cbc8121e481a23ade73
ncarrizoduoc <ni.carrizoc@duocuc.cl>
2026-07-04 02:06:12 -0400

    Se crean clases de configuracion para OpenAPI

---
+++ src/main/java/com/minimarket/minimarket/openapi/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.minimarket.minimarket.openapi.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@Configuration
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT",
+    description = "Autenticacion y autorizacion por roles basada en JWT"
+)
+public class OpenApiConfig {
+
+}
