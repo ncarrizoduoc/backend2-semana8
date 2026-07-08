3068b1d6d93744cc25c2e1e0eb86c484cf16c4d0
ncarrizoduoc <ni.carrizoc@duocuc.cl>
2026-07-08 14:50:54 -0400

    Se elimina import no usado

--- src/test/java/com/minimarket/minimarket/service/UsuarioServiceImplTest.java
+++ src/test/java/com/minimarket/minimarket/service/UsuarioServiceImplTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
