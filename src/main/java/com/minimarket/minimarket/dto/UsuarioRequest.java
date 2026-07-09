7c4718152cc669285217f640bdeb31ed6dd76380
ncarrizoduoc <ni.carrizoc@duocuc.cl>
2026-07-09 03:18:40 -0400

    Se agrega anotacion Builder a UsuarioRequest

--- src/main/java/com/minimarket/minimarket/dto/UsuarioRequest.java
+++ src/main/java/com/minimarket/minimarket/dto/UsuarioRequest.java
@@ -6,12 +6,14 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder()
 @Schema(description = "DTO para ingresar objetos Usuario")
 public class UsuarioRequest {
     @Schema(description = "ID del usuario")
