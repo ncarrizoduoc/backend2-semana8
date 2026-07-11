252e86749d76c0f8a377de074b7faea0e989929b
ncarrizoduoc <ni.carrizoc@duocuc.cl>
2026-07-10 22:37:35 -0400

    Se agregan valores de ejemplo para atributos con Schema

--- src/main/java/com/minimarket/minimarket/security/model/LoginRequest.java
+++ src/main/java/com/minimarket/minimarket/security/model/LoginRequest.java
@@ -1,5 +1,6 @@
 package com.minimarket.minimarket.security.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -8,9 +9,13 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Credenciales de usuario para autenticacion")
 public class LoginRequest {
+    @Schema(description = "Nombre de usuario", example = "user")
     @NotNull(message = "Debe ingresar un nombre de usuario")
     private String username;
+
+    @Schema(description = "Contrasena del usuario", example = "password")
     @NotNull(message = "Debe ingresar una contrasena")
     private String password;
 }
